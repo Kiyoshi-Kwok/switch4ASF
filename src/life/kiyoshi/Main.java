ab14e22db0616afc9fa18b95ff1ed35fa4d85c0e
Kiyoshi_Kwok <academic@kiyoshi.life>
2018-02-05 17:11:16 +0800

    imported JSch
    imported JSch to connect my rpi2 via SSH

--- src/life/kiyoshi/Main.java
+++ src/life/kiyoshi/Main.java
@@ -1,24 +1,20 @@
 package life.kiyoshi;
 
-import java.net.Socket;
-
 /*
  * Create for myself to control rpi2
  */
+
+import com.jcraft.jsch.JSch;
 
 public class Main {
 
     public static void main(String[] args) {
         //some essential variables
-        String ipAdress = "192.168.0.102";
+        String ipAddress = "192.168.0.102";
         String user = "pi";
         String path = "C:/.ssh/rpi2.key";
 
-        //init the socket
-        try {
-            Socket socket = new Socket();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        //use JSch frame to set SSH connection
+        JSch jSch = new JSch();
     }
 }
